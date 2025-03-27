09915d2404f2ca38c6226e1f30a303d07ce39748
jassiel <25399314+vjdv@users.noreply.github.com>
2025-03-26 20:25:08 -0600

    adds missing methods

--- src/main/java/net/vjdv/quickquery/PreparedStatementBuilder.java
+++ src/main/java/net/vjdv/quickquery/PreparedStatementBuilder.java
@@ -2,8 +2,18 @@ package net.vjdv.quickquery;
 
 import net.vjdv.quickquery.exceptions.DataAccessException;
 
+import java.sql.Array;
+import java.sql.Blob;
+import java.sql.Clob;
+import java.sql.Date;
+import java.sql.NClob;
 import java.sql.PreparedStatement;
+import java.sql.Ref;
+import java.sql.RowId;
 import java.sql.SQLException;
+import java.sql.SQLXML;
+import java.sql.Time;
+import java.sql.Timestamp;
 import java.util.function.Function;
 
 /**
@@ -23,17 +33,107 @@ public class PreparedStatementBuilder {
     }
 
     /**
-     * Set a string parameter to consecutive parameter index
+     * Set an array parameter to consecutive parameter index
      *
-     * @param value the string value
+     * @param value the array value
      * @return same PreparedStatementBuilder instance
      */
-    public PreparedStatementBuilder setString(String value) {
+    public PreparedStatementBuilder setArray(Array value) {
         try {
-            stmt.setString(index++, value);
+            stmt.setArray(index++, value);
             return this;
         } catch (SQLException ex) {
-            throw new DataAccessException("Error setting string parameter", ex);
+            throw new DataAccessException("Error setting array parameter", ex);
+        }
+    }
+
+    /**
+     * Set an ascii stream parameter to consecutive parameter index
+     *
+     * @param value the ascii stream value
+     * @return same PreparedStatementBuilder instance
+     */
+    public PreparedStatementBuilder setAsciiStream(java.io.InputStream value) {
+        try {
+            stmt.setAsciiStream(index++, value);
+            return this;
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error setting ascii stream parameter", ex);
+        }
+    }
+
+    /**
+     * Set a big decimal parameter to consecutive parameter index
+     *
+     * @param value the big decimal value
+     * @return same PreparedStatementBuilder instance
+     */
+    public PreparedStatementBuilder setBigDecimal(java.math.BigDecimal value) {
+        try {
+            stmt.setBigDecimal(index++, value);
+            return this;
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error setting bigdecimal parameter", ex);
+        }
+    }
+
+    /**
+     * Set a binary stream parameter to consecutive parameter index
+     *
+     * @param value the binary stream value
+     * @return same PreparedStatementBuilder instance
+     */
+    public PreparedStatementBuilder setBinaryStream(java.io.InputStream value) {
+        try {
+            stmt.setBinaryStream(index++, value);
+            return this;
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error setting binary stream parameter", ex);
+        }
+    }
+
+    /**
+     * Set a blob parameter to consecutive parameter index
+     *
+     * @param value the blob value
+     * @return same PreparedStatementBuilder instance
+     */
+    public PreparedStatementBuilder setBlob(Blob value) {
+        try {
+            stmt.setBlob(index++, value);
+            return this;
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error setting blob parameter", ex);
+        }
+    }
+
+    /**
+     * Set a boolean parameter to consecutive parameter index
+     *
+     * @param value the boolean value
+     * @return same PreparedStatementBuilder instance
+     */
+    public PreparedStatementBuilder setBoolean(boolean value) {
+        try {
+            stmt.setBoolean(index++, value);
+            return this;
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error setting boolean parameter", ex);
+        }
+    }
+
+    /**
+     * Set a byte parameter to consecutive parameter index
+     *
+     * @param value the byte value
+     * @return same PreparedStatementBuilder instance
+     */
+    public PreparedStatementBuilder setByte(byte value) {
+        try {
+            stmt.setByte(index++, value);
+            return this;
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error setting byte parameter", ex);
         }
     }
 
@@ -49,6 +149,81 @@ public class PreparedStatementBuilder {
             return this;
         } catch (SQLException ex) {
             throw new DataAccessException("Error setting bytes parameter", ex);
+        }
+    }
+
+    /**
+     * Set a character stream parameter to consecutive parameter index
+     *
+     * @param value the character stream value
+     * @return same PreparedStatementBuilder instance
+     */
+    public PreparedStatementBuilder setCharacterStream(java.io.Reader value) {
+        try {
+            stmt.setCharacterStream(index++, value);
+            return this;
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error setting character stream parameter", ex);
+        }
+    }
+
+    /**
+     * Set a clob parameter to consecutive parameter index
+     *
+     * @param value the clob value
+     * @return same PreparedStatementBuilder instance
+     */
+    public PreparedStatementBuilder setClob(Clob value) {
+        try {
+            stmt.setClob(index++, value);
+            return this;
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error setting clob parameter", ex);
+        }
+    }
+
+    /**
+     * Set a date parameter to consecutive parameter index
+     *
+     * @param value the date value
+     * @return same PreparedStatementBuilder instance
+     */
+    public PreparedStatementBuilder setDate(Date value) {
+        try {
+            stmt.setDate(index++, value);
+            return this;
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error setting date parameter", ex);
+        }
+    }
+
+    /**
+     * Set a double parameter to consecutive parameter index
+     *
+     * @param value the double value
+     * @return same PreparedStatementBuilder instance
+     */
+    public PreparedStatementBuilder setDouble(double value) {
+        try {
+            stmt.setDouble(index++, value);
+            return this;
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error setting double parameter", ex);
+        }
+    }
+
+    /**
+     * Set a float parameter to consecutive parameter index
+     *
+     * @param value the float value
+     * @return same PreparedStatementBuilder instance
+     */
+    public PreparedStatementBuilder setFloat(float value) {
+        try {
+            stmt.setFloat(index++, value);
+            return this;
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error setting float parameter", ex);
         }
     }
 
@@ -79,6 +254,232 @@ public class PreparedStatementBuilder {
             return this;
         } catch (SQLException ex) {
             throw new DataAccessException("Error setting long parameter", ex);
+        }
+    }
+
+    /**
+     * Set a ncharacter stream parameter to consecutive parameter index
+     *
+     * @param value the ncharacter stream value
+     * @return same PreparedStatementBuilder instance
+     */
+    public PreparedStatementBuilder setNCharacterStream(java.io.Reader value) {
+        try {
+            stmt.setNCharacterStream(index++, value);
+            return this;
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error setting ncharacter stream parameter", ex);
+        }
+    }
+
+    /**
+     * Set a nclob parameter to consecutive parameter index
+     *
+     * @param value the nclob value
+     * @return same PreparedStatementBuilder instance
+     */
+    public PreparedStatementBuilder setNClob(NClob value) {
+        try {
+            stmt.setNClob(index++, value);
+            return this;
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error setting nclob parameter", ex);
+        }
+    }
+
+    /**
+     * Set a nclob parameter to consecutive parameter index
+     *
+     * @param value the nclob value
+     * @return same PreparedStatementBuilder instance
+     */
+    public PreparedStatementBuilder setNClob(java.io.Reader value) {
+        try {
+            stmt.setNClob(index++, value);
+            return this;
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error setting nclob parameter", ex);
+        }
+    }
+
+    /**
+     * Set a nstring parameter to consecutive parameter index
+     *
+     * @param value the nstring value
+     * @return same PreparedStatementBuilder instance
+     */
+    public PreparedStatementBuilder setNString(String value) {
+        try {
+            stmt.setNString(index++, value);
+            return this;
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error setting nstring parameter", ex);
+        }
+    }
+
+    /**
+     * Set a null parameter to consecutive parameter index
+     *
+     * @param sqlType the sql type
+     * @return same PreparedStatementBuilder instance
+     */
+    public PreparedStatementBuilder setNull(int sqlType) {
+        try {
+            stmt.setNull(index++, sqlType);
+            return this;
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error setting null parameter", ex);
+        }
+    }
+
+    /**
+     * Set a null parameter to consecutive parameter index
+     *
+     * @param sqlType  the sql type
+     * @param typeName the type name
+     * @return same PreparedStatementBuilder instance
+     */
+    public PreparedStatementBuilder setNull(int sqlType, String typeName) {
+        try {
+            stmt.setNull(index++, sqlType, typeName);
+            return this;
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error setting null parameter", ex);
+        }
+    }
+
+    /**
+     * Set an object parameter to consecutive parameter index
+     *
+     * @param value the object value
+     * @return same PreparedStatementBuilder instance
+     */
+    public PreparedStatementBuilder setObject(Object value) {
+        try {
+            stmt.setObject(index++, value);
+            return this;
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error setting object parameter", ex);
+        }
+    }
+
+    /**
+     * Set an ref parameter to consecutive parameter index
+     *
+     * @param value the ref value
+     * @return same PreparedStatementBuilder instance
+     */
+    public PreparedStatementBuilder setRef(Ref value) {
+        try {
+            stmt.setRef(index++, value);
+            return this;
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error setting ref parameter", ex);
+        }
+    }
+
+    /**
+     * Set a rowid parameter to consecutive parameter index
+     *
+     * @param value the rowid value
+     * @return same PreparedStatementBuilder instance
+     */
+    public PreparedStatementBuilder setRowId(RowId value) {
+        try {
+            stmt.setRowId(index++, value);
+            return this;
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error setting rowid parameter", ex);
+        }
+    }
+
+    /**
+     * Set a short parameter to consecutive parameter index
+     *
+     * @param value the short value
+     * @return same PreparedStatementBuilder instance
+     */
+    public PreparedStatementBuilder setShort(short value) {
+        try {
+            stmt.setShort(index++, value);
+            return this;
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error setting short parameter", ex);
+        }
+    }
+
+    /**
+     * Set a sqlxml parameter to consecutive parameter index
+     *
+     * @param value the sqlxml value
+     * @return same PreparedStatementBuilder instance
+     */
+    public PreparedStatementBuilder setSQLXML(SQLXML value) {
+        try {
+            stmt.setSQLXML(index++, value);
+            return this;
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error setting sqlxml parameter", ex);
+        }
+    }
+
+    /**
+     * Set a string parameter to consecutive parameter index
+     *
+     * @param value the string value
+     * @return same PreparedStatementBuilder instance
+     */
+    public PreparedStatementBuilder setString(String value) {
+        try {
+            stmt.setString(index++, value);
+            return this;
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error setting string parameter", ex);
+        }
+    }
+
+    /**
+     * Set a time parameter to consecutive parameter index
+     *
+     * @param value the time value
+     * @return same PreparedStatementBuilder instance
+     */
+    public PreparedStatementBuilder setTime(Time value) {
+        try {
+            stmt.setTime(index++, value);
+            return this;
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error setting time parameter", ex);
+        }
+    }
+
+    /**
+     * Set a timestamp parameter to consecutive parameter index
+     *
+     * @param value the timestamp value
+     * @return same PreparedStatementBuilder instance
+     */
+    public PreparedStatementBuilder setTimestamp(Timestamp value) {
+        try {
+            stmt.setTimestamp(index++, value);
+            return this;
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error setting timestamp parameter", ex);
+        }
+    }
+
+    /**
+     * Set a url parameter to consecutive parameter index
+     *
+     * @param value the url value
+     * @return same PreparedStatementBuilder instance
+     */
+    public PreparedStatementBuilder setURL(java.net.URL value) {
+        try {
+            stmt.setURL(index++, value);
+            return this;
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error setting url parameter", ex);
         }
     }
 
--- src/main/java/net/vjdv/quickquery/ResultSetWrapper.java
+++ src/main/java/net/vjdv/quickquery/ResultSetWrapper.java
@@ -2,8 +2,11 @@ package net.vjdv.quickquery;
 
 import net.vjdv.quickquery.exceptions.DataAccessException;
 
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Time;
+import java.sql.Timestamp;
 
 /**
  * Wrapper for ResultSet to mute exceptions, throws DataAccessException instead when reading values
@@ -21,6 +24,15 @@ public class ResultSetWrapper {
     }
 
     /**
+     * Returns the wrapped ResultSet
+     *
+     * @return ResultSet
+     */
+    public ResultSet getResultSet() {
+        return rs;
+    }
+
+    /**
      * Moves the cursor to the next row
      *
      * @return true if there is a next row
@@ -30,6 +42,66 @@ public class ResultSetWrapper {
             return rs.next();
         } catch (SQLException ex) {
             throw new DataAccessException("Error getting next result", ex);
+        }
+    }
+
+    /**
+     * Retrieves the value of the designated column in the current row of this ResultSet object as a boolean.
+     *
+     * @param column column name
+     * @return the column value; if the value is SQL NULL, the value returned is false
+     * @throws DataAccessException if a SQLException occurs
+     */
+    public boolean getBoolean(String column) {
+        try {
+            return rs.getBoolean(column);
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error getting boolean from column " + column, ex);
+        }
+    }
+
+    /**
+     * Retrieves the value of the designated column in the current row of this ResultSet object as a boolean.
+     *
+     * @param column column index
+     * @return the column value; if the value is SQL NULL, the value returned is false
+     * @throws DataAccessException if a SQLException occurs
+     */
+    public boolean getBoolean(int column) {
+        try {
+            return rs.getBoolean(column);
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error getting boolean from column " + column, ex);
+        }
+    }
+
+    /**
+     * Retrieves the value of the designated column in the current row of this ResultSet object as a byte.
+     *
+     * @param column column name
+     * @return the column value; if the value is SQL NULL, the value returned is 0
+     * @throws DataAccessException if a SQLException occurs
+     */
+    public byte getByte(String column) {
+        try {
+            return rs.getByte(column);
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error getting byte from column " + column, ex);
+        }
+    }
+
+    /**
+     * Retrieves the value of the designated column in the current row of this ResultSet object as a byte.
+     *
+     * @param column column index
+     * @return the column value; if the value is SQL NULL, the value returned is 0
+     * @throws DataAccessException if a SQLException occurs
+     */
+    public byte getByte(int column) {
+        try {
+            return rs.getByte(column);
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error getting byte from column " + column, ex);
         }
     }
 
@@ -60,6 +132,96 @@ public class ResultSetWrapper {
             return rs.getBytes(column);
         } catch (SQLException ex) {
             throw new DataAccessException("Error getting bytes from column " + column, ex);
+        }
+    }
+
+    /**
+     * Retrieves the value of the designated column in the current row of this ResultSet object as a Date object.
+     *
+     * @param column column name
+     * @return the column value; if the value is SQL NULL, the value returned is null
+     * @throws DataAccessException if a SQLException occurs
+     */
+    public Date getDate(String column) {
+        try {
+            return rs.getDate(column);
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error getting date from column " + column, ex);
+        }
+    }
+
+    /**
+     * Retrieves the value of the designated column in the current row of this ResultSet object as a Date object.
+     *
+     * @param column column index
+     * @return the column value; if the value is SQL NULL, the value returned is null
+     * @throws DataAccessException if a SQLException occurs
+     */
+    public Date getDate(int column) {
+        try {
+            return rs.getDate(column);
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error getting date from column " + column, ex);
+        }
+    }
+
+    /**
+     * Retrieves the value of the designated column in the current row of this ResultSet object as a double.
+     *
+     * @param column column name
+     * @return the column value; if the value is SQL NULL, the value returned is 0.0
+     * @throws DataAccessException if a SQLException occurs
+     */
+    public double getDouble(String column) {
+        try {
+            return rs.getDouble(column);
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error getting double from column " + column, ex);
+        }
+    }
+
+    /**
+     * Retrieves the value of the designated column in the current row of this ResultSet object as a double.
+     *
+     * @param column column index
+     * @return the column value; if the value is SQL NULL, the value returned is 0.0
+     * @throws DataAccessException if a SQLException occurs
+     */
+    public double getDouble(int column) {
+        try {
+            return rs.getDouble(column);
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error getting double from column " + column, ex);
+        }
+    }
+
+    /**
+     * Retrieves the value of the designated column in the current row of this ResultSet object as a float.
+     *
+     * @param column column name
+     * @return the column value; if the value is SQL NULL, the value returned is 0.0f
+     * @throws DataAccessException if a SQLException occurs
+     */
+    public float getFloat(String column) {
+        try {
+            return rs.getFloat(column);
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error getting float from column " + column, ex);
+        }
+    }
+
+    /**
+     * Retrieves the value of the designated column in the current row of this ResultSet object as a float.
+     *
+     * @param column column index
+     * @return the column value; if the value is SQL NULL, the value returned is 0.0f
+     * @throws DataAccessException if a SQLException occurs
+     */
+    public float getFloat(int column) {
+        try {
+            return rs.getFloat(column);
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error getting float from column " + column, ex);
         }
     }
 
@@ -150,6 +312,146 @@ public class ResultSetWrapper {
             return rs.getLong(column);
         } catch (SQLException ex) {
             throw new DataAccessException("Error getting long from column " + column, ex);
+        }
+    }
+
+    /**
+     * Retrieves the value of the designated column in the current row of this ResultSet object as an Object.
+     *
+     * @param column column index
+     * @return the column value; if the value is SQL NULL, the value returned is null
+     * @throws DataAccessException if a SQLException occurs
+     */
+    public Object getObject(int column) {
+        try {
+            return rs.getObject(column);
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error getting object from column " + column, ex);
+        }
+    }
+
+    /**
+     * Retrieves the value of the designated column in the current row of this ResultSet object as an Object of the specified type.
+     *
+     * @param column column name
+     * @param type   Class of the type to retrieve
+     * @param <T>    the type of the object
+     * @return the column value; if the value is SQL NULL, the value returned is null
+     * @throws DataAccessException if a SQLException occurs
+     */
+    public <T> T getObject(String column, Class<T> type) {
+        try {
+            return rs.getObject(column, type);
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error getting object from column " + column, ex);
+        }
+    }
+
+    /**
+     * Retrieves the value of the designated column in the current row of this ResultSet object as an Object of the specified type.
+     *
+     * @param column column index
+     * @param type   Class of the type to retrieve
+     * @param <T>    the type of the object
+     * @return the column value; if the value is SQL NULL, the value returned is null
+     * @throws DataAccessException if a SQLException occurs
+     */
+    public <T> T getObject(int column, Class<T> type) {
+        try {
+            return rs.getObject(column, type);
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error getting object from column " + column, ex);
+        }
+    }
+
+
+    /**
+     * Retrieves the value of the designated column in the current row of this ResultSet object as a short.
+     *
+     * @param column column name
+     * @return the column value; if the value is SQL NULL, the value returned is 0
+     * @throws DataAccessException if a SQLException occurs
+     */
+    public short getShort(String column) {
+        try {
+            return rs.getShort(column);
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error getting short from column " + column, ex);
+        }
+    }
+
+    /**
+     * Retrieves the value of the designated column in the current row of this ResultSet object as a short.
+     *
+     * @param column column index
+     * @return the column value; if the value is SQL NULL, the value returned is 0
+     * @throws DataAccessException if a SQLException occurs
+     */
+    public short getShort(int column) {
+        try {
+            return rs.getShort(column);
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error getting short from column " + column, ex);
+        }
+    }
+
+    /**
+     * Retrieves the value of the designated column in the current row of this ResultSet object as a Time object.
+     *
+     * @param column column name
+     * @return the column value; if the value is SQL NULL, the value returned is null
+     * @throws DataAccessException if a SQLException occurs
+     */
+    public Time getTime(String column) {
+        try {
+            return rs.getTime(column);
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error getting time from column " + column, ex);
+        }
+    }
+
+    /**
+     * Retrieves the value of the designated column in the current row of this ResultSet object as a Time object.
+     *
+     * @param column column index
+     * @return the column value; if the value is SQL NULL, the value returned is null
+     * @throws DataAccessException if a SQLException occurs
+     */
+    public Time getTime(int column) {
+        try {
+            return rs.getTime(column);
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error getting time from column " + column, ex);
+        }
+    }
+
+    /**
+     * Retrieves the value of the designated column in the current row of this ResultSet object as a Timestamp object.
+     *
+     * @param column column name
+     * @return the column value; if the value is SQL NULL, the value returned is null
+     * @throws DataAccessException if a SQLException occurs
+     */
+    public Timestamp getTimestamp(String column) {
+        try {
+            return rs.getTimestamp(column);
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error getting timestamp from column " + column, ex);
+        }
+    }
+
+    /**
+     * Retrieves the value of the designated column in the current row of this ResultSet object as a Timestamp object.
+     *
+     * @param column column index
+     * @return the column value; if the value is SQL NULL, the value returned is null
+     * @throws DataAccessException if a SQLException occurs
+     */
+    public Timestamp getTimestamp(int column) {
+        try {
+            return rs.getTimestamp(column);
+        } catch (SQLException ex) {
+            throw new DataAccessException("Error getting timestamp from column " + column, ex);
         }
     }
 }
